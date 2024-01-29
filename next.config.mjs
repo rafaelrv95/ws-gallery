63069063abaca5b78d92cda9e4998f5055242a30
Rafael Rodriguez <rafaelrv@Rafaels-MacBook-Air.local>
2024-01-29 16:53:18 -0500

    merge

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     images: {
         domains: ['geeky.com.ar', 'i.abcnewsfe.com', 'c2.staticflickr.com', 's3.amazonaws.com/photos.theojoproject.com'],
       },
